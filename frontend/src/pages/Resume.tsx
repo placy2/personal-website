8a2329aa307a8b6a10feb6d0aa6c96466f7d61c5
Parker Lacy <parkerlacy@taras-air.mynetworksettings.com>
2025-02-15 09:05:02 -0700

    Cleaned up text formatting for readability and added logos

--- frontend/src/pages/Resume.tsx
+++ frontend/src/pages/Resume.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Resume: React.FC = () => {
-  return <h1>Resume Page</h1>;
+  return (
+  <div>
+    <h1>Resume Page</h1>
+    <h2>Under Construction - reach out directly if you'd like an up-to-date copy.</h2>
+  </div>
+  );
 };
 
 export default Resume;
